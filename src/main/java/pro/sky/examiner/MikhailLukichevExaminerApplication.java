9f1377f1274c652c148d663c822e34722dcd86a3
Mikhail Lukichev <dextrumm@gmail.com>
2023-11-13 19:37:45 -0700

    Initial commit

---
+++ src/main/java/pro/sky/examiner/MikhailLukichevExaminerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.examiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikhailLukichevExaminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikhailLukichevExaminerApplication.class, args);
+	}
+
+}
